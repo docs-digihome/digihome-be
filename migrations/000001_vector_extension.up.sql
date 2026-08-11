ab093ed36469bc6ec7408bc82fd0aee3d3f01037
daffadon <daffaputranarendra9@gmail.com>
2026-08-11 14:23:01 +0800

    chore(migrations): remove page_number column

--- migrations/000001_vector_extension.up.sql
+++ migrations/000001_vector_extension.up.sql
@@ -2,7 +2,6 @@ CREATE EXTENSION IF NOT EXISTS vector;
 
 CREATE TABLE document_chunks (
   id UUID PRIMARY KEY DEFAULT uuidv7(),
-  page_number INTEGER,
   chunk_index INTEGER NOT NULL,
   content TEXT NOT NULL,
   embedding vector(1024),
