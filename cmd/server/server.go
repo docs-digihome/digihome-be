18cd3e38f4c1a7111a35cb6a3dadad119fa955d6
daffadon <daffaputranarendra9@gmail.com>
2026-08-11 14:25:25 +0800

    refactor(server): change to configurable port

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -9,11 +9,12 @@ import (
 	"github.com/daffadon/digihome/internal/constant"
 	s3_infra "github.com/daffadon/digihome/internal/infra/s3"
 	"github.com/go-chi/chi/v5"
+	"github.com/spf13/viper"
 	"go.uber.org/fx"
 )
 
 func RunHTTPServer(lc fx.Lifecycle, r chi.Router, slog *slog.Logger, s s3_infra.RustfsInfra) *http.Server {
-	srv := &http.Server{Addr: ":8080", Handler: r}
+	srv := &http.Server{Addr: ":" + viper.GetString("app.http.port"), Handler: r}
 	err := s.InitBucket(context.Background(), constant.APP_BUCKET)
 	if err != nil {
 		slog.Error("Failed to init bucket", "error", err)
