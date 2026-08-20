cf7c0ff7dd5b3f03160adac26cf496287defbc03
daffadon <daffaputranarendra9@gmail.com>
2026-08-20 17:47:41 +0800

    feat(schema): add batch insert document response

--- internal/schema/rag.go
+++ internal/schema/rag.go
@@ -6,3 +6,11 @@ type (
 		ObjectKey string
 	}
 )
+
+type (
+	BatchInsertDocumentResponse struct {
+		ObjectKey    string `json:"object_key"`
+		OriginalName string `json:"original_name"`
+		Error        string `json:"error,omitempty"`
+	}
+)
